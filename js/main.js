de77f30d33439d38f44c0a6c8a208cb005dfe1cd
Jakub <default@defaults-mbp.home>
2017-04-20 02:37:00 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[3];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length = 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("Thats why I love you");
+	}
+else if (cardsInPlay[0] !== cardsInPlay[1]) {
+	alert("You failure of a human being. You failed!");
+}
+}
 
 
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardFour);
+
+
+
+
+console.log("User flipped king");
+
+
+console.log("User flipped queen");
+
+
+
+
