8183a4c7293e1ef835623cc53209bb1795af2021
Jakub <default@defaults-mbp.home>
2017-04-21 22:58:16 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suitarts: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}
+
+];
+
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -15,12 +40,14 @@ if (cardsInPlay.length === 2) {
 
 var flipCard = function (cardId) {
 	
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 }
 
-flipCard(2);
+flipCard(1);
 flipCard(3);
 
 
